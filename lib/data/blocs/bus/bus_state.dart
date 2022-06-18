9cb03f4727cb03b69ea69b402caf2c126a8a9f38
Darwin Mamani <darwin_xd10@hotmail.com>
2022-06-18 18:13:59 -0400

    darwin, xd

--- lib/data/blocs/bus/bus_state.dart
+++ lib/data/blocs/bus/bus_state.dart
@@ -1,6 +1,7 @@
 part of 'bus_bloc.dart';
 
 class BusState extends Equatable {
+
   final Map<String, Set<Polyline>> routes;
 
   const BusState({required this.routes});
@@ -9,7 +10,7 @@ class BusState extends Equatable {
       BusState(routes: routes ?? this.routes);
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [routes];
 
   @override
   String toString() => '{routes: $routes}';
