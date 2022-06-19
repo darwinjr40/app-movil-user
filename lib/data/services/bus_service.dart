d2cb52450b41acbbf6124d4b2dbe433423e8864c
Ing-Harold <Ing-Harold>
2022-06-19 14:30:19 -0400

    colores actualizados hyun

--- lib/data/services/bus_service.dart
+++ lib/data/services/bus_service.dart
@@ -15,6 +15,19 @@ class BusService {
     Polyline lineaRutaVuelta;
     Set<Polyline> linea;
     routes = {};
+    List<Color> colorLineas = [
+      Colors.red.withOpacity(0.7), Colors.red, //1
+      Colors.blue.withOpacity(0.7), Colors.blue, //2
+      Colors.green.withOpacity(0.7), Colors.green, //5
+      Colors.deepPurple.withOpacity(0.7), Colors.deepPurple, //8
+      Colors.pink.withOpacity(0.7), Colors.pink, //9
+      Colors.brown.withOpacity(0.7), Colors.brown, //10
+      Colors.teal.withOpacity(0.7), Colors.teal, //11
+      Colors.indigo.withOpacity(0.7), Colors.indigo, //16
+      Colors.orange.withOpacity(0.7), Colors.orange, //17
+      Colors.yellowAccent.withOpacity(0.7), Colors.yellowAccent, //18
+    ];
+    int i = 0;
     for (var item in decodedResp) {
       //recorre todos los buses
       linea = {};
@@ -27,25 +40,28 @@ class BusService {
       }
       lineaRutaIda = Polyline(
         polylineId: PolylineId('ln' + a + 'Ida'),
-        color: Colors.red.withOpacity(0.7),
+        // color: Colors.red.withOpacity(0.7),
+        color: colorLineas[i],
         width: 3,
         startCap: Cap.roundCap,
         endCap: Cap.roundCap,
         points: rutaIda,
       );
-
+      i++;
       for (var vuelta in item["paths"]["vuelta"]) {
         rutaVuelta.add(LatLng(double.parse((vuelta["latitude"])),
             double.parse(vuelta["longitude"])));
       }
       lineaRutaVuelta = Polyline(
         polylineId: PolylineId('ln' + a + 'Vuelta'),
-        color: Colors.red,
+        // color: Colors.red,
+        color: colorLineas[i],
         width: 3,
         startCap: Cap.roundCap,
         endCap: Cap.roundCap,
         points: rutaVuelta,
       );
+      i++;
       linea.add(lineaRutaIda);
       linea.add(lineaRutaVuelta);
       // print(a);
