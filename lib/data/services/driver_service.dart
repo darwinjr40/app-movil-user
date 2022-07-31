33792c44f33e79c514cfc195a70b93e51b3174f6
Ing-Harold <Ing-Harold>
2022-07-31 11:47:26 -0400

    ultimo cambio

--- lib/data/services/driver_service.dart
+++ lib/data/services/driver_service.dart
@@ -8,9 +8,7 @@ class DriverService extends ChangeNotifier {
   // final String _baseUrl = "supportficct.ga/sig_backend/public/api/";
 
   Future<List<Drivers>> getDrivers(int busID, double lat, double lon) async {
-    debugPrint('entrando a la APIIIIIIIIIIIIIIIIIII');
-    // var url = Uri.https(_baseUrl, '/drivers/nearbuses/1/-17.77987129557522/-63.17502205921347');
-    // final resp = await http.get(url);
+
     var resp = await http.get(Uri.parse(
         'https://supportficct.ga/sig_backend/public/api/drivers/nearbuses/$busID/$lat/$lon'));
     var jsonResp = jsonDecode(resp.body);
