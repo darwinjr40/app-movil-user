2be78b3257805609a218ebbf558e6618904cda79
darwin mamani <darwin_xd10@hotmail.com>
2022-12-29 05:09:13 -0400

    darwin first

--- lib/data/services/driver_service.dart
+++ lib/data/services/driver_service.dart
@@ -3,23 +3,21 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:micros_user_app/data/models/models.dart';
+import 'package:micros_user_app/data/services/services.dart';
 
 class DriverService extends ChangeNotifier {
   // final String _baseUrl = "supportficct.ga/sig_backend/public/api/";
 
   Future<List<Drivers>> getDrivers(int busID, double lat, double lon) async {
     debugPrint("class DriverService:getDrivers($busID/$lat/$lon)");
-    var resp = await http.get(Uri.parse(
-        'https://supportficct.ga/sig_backend/public/api/drivers/nearbuses/$busID/$lat/$lon'));
-    var jsonResp = jsonDecode(resp.body);
     List<Drivers> listaDriver = [];
-
+    final url = Env.baseUrl + 'drivers/nearbuses/$busID/$lat/$lon';
+    var resp = await http.get(Uri.parse(url));
+    var jsonResp = jsonDecode(resp.body);
+    
     for (var item in jsonResp) {
       Drivers driver = Drivers.fromMap(item);
       listaDriver.add(driver);
-    }
-    if (listaDriver.isEmpty) {
-      return [];
     }
     debugPrint('LISTA CoN DRIVERS');
     debugPrint(listaDriver.length.toString());
---
+++ lib/data/services/env.dart
@@ -0,0 +1,5 @@
+class Env {
+  // static const String baseUrl = 'https://supportficct.ga/sig_backend/api/';
+  static const String baseUrl = 'http://192.168.1.2/sw1-project/public/api/';
+
+}
