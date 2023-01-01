6517bdd23959579c0fe12f0e0c7743d802130b59
darwin mamani <darwin_xd10@hotmail.com>
2023-01-01 15:29:10 -0400

    services push notifications v1

--- lib/data/services/push_notifications_service.dart
+++ lib/data/services/push_notifications_service.dart
@@ -1,19 +1,42 @@
 //5A:2F:E8:7B:66:B2:C0:25:FA:98:DB:08:14:F3:40:03:9E:EE:99:48
 
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 
-// class PushNotificationService {
+class PushNotificationService {
 
-//   final FirebaseMessaging _firebaseMessaging =  FirebaseMessaging.instance;
-//   final StreamController _streamController =
-//     StreamController<Map<String, dynamic>>.broadcast();
+  // final FirebaseMessaging _firebaseMessaging =  FirebaseMessaging.instance;
+  final StreamController _streamController =
+    StreamController<Map<String, dynamic>>.broadcast();
 
-//   Stream<dynamic> get message => _streamController.stream;
+  Stream<dynamic> get message => _streamController.stream;
 
 
-//   void initPushNotifications() {
+  void initPushNotifications() {
+    //onMessage
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      Map<String, dynamic> data = message.data;
+        debugPrint('Cuando estamos en primer plano');
+        debugPrint('OnMessage: $data');
+        _streamController.sink.add(data);
+    });
+  //onResume
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      debugPrint('A new onMessageOpenedApp event was published!');
+      Map<String, dynamic> data = message.data;
+      debugPrint('OnResume $data');
+      _streamController.sink.add(data);
+      // Navigator.pushNamed(
+      //   context,
+      //   '/message',
+      //   arguments: MessageArguments(message, true),
+      // );
+    });
+
 //     _firebaseMessaging.configure(
 //       onMessage: (Map<String, dynamic> message) {
 //         print('Cuando estamos en primer plano');
@@ -42,10 +65,10 @@
 //       print('Coonfiguraciones para Ios fueron regustradas $settings');
 //     });
 
-//   }
+  }
 
-//   void dispose () {
-//     _streamController?.onCancel;
-//   }
+  void dispose () {
+    _streamController.onCancel;
+  }
 
-// }
+}
